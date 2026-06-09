10c63bab657952830b768474822f5ab21254301f
Mylambigai <mylambigai16@gmail.com>
2026-06-09 09:02:08 +0530

    Requirement.txt

--- Python_selenium/Pytest_Demo/first_test.py
+++ Python_selenium/Pytest_Demo/first_test.py
@@ -4,11 +4,11 @@ import pytest
 def test_sample1():
     print("Hai")
     assert "myl".__eq__("my ")
-
+ 
 @pytest.mark.xfail(reason="Just skip")
 def test_sample2():
     print("Helloo")
-    assert 1+1 < 3
+    assert 1+1 < 3  
 
 @pytest.mark.parametrize("a1,a2",[(1,2),(1,2),(1,2)])
 def test_sample3(a1,a2):
